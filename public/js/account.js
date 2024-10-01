a488554dc2da78a7ede215b85915b467d3e9ed67
IgnisAlienus <andrewhuntkvfd@gmail.com>
2024-10-01 13:36:56 -0500

    update: account WIP

--- public/js/account.js
+++ public/js/account.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', () => {
   console.log(user);
+
+  // Get element by id
+  const accountP = document.getElementById('account-p');
+  accountP.textContent = `Welcome, ${user.nickname}!<br>This page is still a work in progress.`;
 });
